b50f0dd4d26c755d3188dfbfd316f119205b8daa
Jeremy Leipzig <leipzig@gmail.com>
2020-06-12 10:30:53 -0400

    wordcloud

--- src/wordcloud.R
+++ src/wordcloud.R
@@ -4,10 +4,28 @@ library(tidytext)
 library(stringr)
 library(wordcloud)
 library(knitr)
+library(readr)
 
 pal <- brewer.pal(8,"Dark2")
-path<-"~/Downloads/References from .bib"
-df <- bib2df(path)
-df %>% dplyr::filter(!is.na(ABSTRACT)) %>% unnest_tokens(word,ABSTRACT) %>% anti_join(stop_words) %>% count(word, sort = TRUE) %>% ungroup() -> tokens_clean
+
+useFullText<-TRUE
+
+if(useFullText==TRUE){
+  #full text from pdfs
+  readr::read_file("../data/citations/tokens.txt.gz") %>% 
+    stringr::str_replace_all("'","") %>% 
+    stringr::str_replace_all("\\[","") %>% 
+    stringr::str_replace_all("\\]","") %>% 
+    stringr::str_replace_all(" ","") %>% 
+    stringr::str_split(pattern=',') %>%
+    stringr::str_to_lower() -> tokenvec
+    data.frame(word=tokenvec[[1]]) %>% anti_join(stop_words) %>% count(word, sort = TRUE) %>% ungroup() -> tokens_clean
+}else{
+  #just the abstracts
+  path<-"../data/citations/metadata-in-rcr-refs.bib"
+  df <- bib2df(path)
+  df %>% dplyr::filter(!is.na(ABSTRACT)) %>% unnest_tokens(word,ABSTRACT) %>% anti_join(stop_words) %>% count(word, sort = TRUE) %>% ungroup() -> tokens_clean
+}
+
 tokens_clean %>%
 with(wordcloud(word, n, random.order = FALSE, max.words = 100, colors=pal))
